76787219fbc2533a8a608f4bc356b09db2d10f82
Marko Savic <marko.s.savic@saga.rs>
2020-10-16 13:00:17 +0200

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { FluentCustomizations } from '@uifabric/fluent-theme';
-import { Customizer, mergeStyles } from 'office-ui-fabric-react';
-import * as serviceWorker from './serviceWorker';
-
-// Inject some global styles
-mergeStyles({
-  selectors: {
-    ':global(body), :global(html), :global(#root)': {
-      margin: 0,
-      padding: 0,
-      height: '100vh'
-    }
-  }
-});
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/css/default.css'
+import { App } from './main'
 
 ReactDOM.render(
-  <Customizer {...FluentCustomizations}>
+  <React.StrictMode>
     <App />
-  </Customizer>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
